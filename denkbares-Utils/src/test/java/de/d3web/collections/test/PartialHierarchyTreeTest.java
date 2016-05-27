1e98337dd32272341f621a197c899a69fdc323e6
jochenreutelshofer <jochen.reutelshoefer@denkbares.com>
2016-05-27 11:57:53 +0200

    re-added tests that somehow got lost during refactoring or git migration

--- denkbares-Utils/src/test/java/de/d3web/collections/test/PartialHierarchyTreeTest.java
+++ denkbares-Utils/src/test/java/de/d3web/collections/test/PartialHierarchyTreeTest.java
@@ -1,8 +1,202 @@
 package de.d3web.collections.test;
 
-/**
- * @author Albrecht Striffler (denkbares GmbH)
- * @created 23.05.16
- */
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Test;
+
+import de.d3web.collections.PartialHierarchyException;
+import de.d3web.collections.PartialHierarchyTree;
+import de.d3web.collections.PartialHierarchyTree.Node;
+
+import static org.junit.Assert.*;
+
 public class PartialHierarchyTreeTest {
+
+	@Test
+	public void testHierarchyBasicInsert() {
+		PartialHierarchyTree<String> tree = new PartialHierarchyTree<String>(
+				new StringPrefixHierarchy());
+
+		assertEquals(0, tree.getNodeCount());
+
+		String a = "A";
+		String b = "B";
+		String ba = "BA";
+
+		// insert A
+		insert(tree, a);
+
+		assertEquals(1, tree.getNodeCount());
+
+		// insert B
+		insert(tree, b);
+
+		assertEquals(2, tree.getNodeCount());
+		assertEquals(2, tree.getNodes().size());
+
+		Node<String> aNode = tree.find(a);
+		assertTrue(aNode != null);
+		assertTrue(aNode.getData().equals(a));
+		assertEquals(0, aNode.getChildren().size());
+
+		Node<String> bNode = tree.find(b);
+		assertTrue(bNode != null);
+		assertTrue(bNode.getData().equals(b));
+		assertEquals(0, bNode.getChildren().size());
+
+		// insert BA
+		insert(tree,ba);
+		assertEquals(3, tree.getNodeCount());
+		assertEquals(3, tree.getNodes().size());
+
+		Node<String> baNode = tree.find(ba);
+		assertTrue(baNode != null);
+		assertTrue(baNode.getData().equals(ba));
+		assertEquals(0, baNode.getChildren().size());
+		assertTrue(baNode.getParent().getData().equals(b));
+
+		// b should have BA as child now
+		bNode = tree.find(b);
+		assertTrue(bNode != null);
+		assertTrue(bNode.getData().equals(b));
+		List<Node<String>> bChildren = bNode.getChildren();
+		assertEquals(1, bChildren.size());
+		assertTrue(bChildren.contains(new Node<String>(ba)));
+
+		// A should be still alone
+		aNode = tree.find(a);
+		assertTrue(aNode != null);
+		assertTrue(aNode.getData().equals(a));
+		assertEquals(0, aNode.getChildren().size());
+
+		// test leafs
+		final Collection<String> leafNodes = tree.getLeafNodes();
+		assertEquals(2, leafNodes.size());
+		assertTrue(leafNodes.contains(a));
+		assertTrue(leafNodes.contains(ba));
+
+	}
+
+	@Test
+	public void testHierarchyRestructuring1() {
+		PartialHierarchyTree<String> tree = new PartialHierarchyTree<String>(
+				new StringPrefixHierarchy());
+
+		String b = "B";
+		String ba = "BA";
+		String bac = "BAC";
+
+		insert(tree,ba);
+		insert(tree,bac);
+		insert(tree, b);
+
+		// check correct insertion
+		assertEquals(3, tree.getNodeCount());
+
+		Node<String> baNode = tree.find(ba);
+		assertTrue(baNode.getParent().equals(tree.find(b)));
+		List<Node<String>> baChildren = baNode.getChildren();
+		assertEquals(1, baChildren.size());
+		assertTrue(baChildren.contains(tree.find(bac)));
+
+		// remove ba
+		boolean removed = tree.remove(ba);
+		assertTrue(removed);
+		assertFalse(tree.remove(ba));
+		assertEquals(null, tree.find(ba));
+
+		// then BAC should be child of B
+		assertEquals(2, tree.getNodeCount());
+		Node<String> bacNode = tree.find(bac);
+		Node<String> bNode = tree.find(b);
+		List<Node<String>> bChildren = bNode.getChildren();
+		assertEquals(1, bChildren.size());
+		assertTrue(bChildren.contains(bacNode));
+		assertTrue(bacNode.getParent().equals(bNode));
+
+		// test leafs
+		final Collection<String> leafNodes = tree.getLeafNodes();
+		assertEquals(1, leafNodes.size());
+		assertTrue(leafNodes.contains(bac));
+	}
+
+	@Test
+	public void testHierarchyRestructuring2() {
+		PartialHierarchyTree<String> tree = new PartialHierarchyTree<String>(
+				new StringPrefixHierarchy());
+
+		String b = "B";
+		String ba = "BA";
+		String bi = "BI";
+
+		insert(tree,ba);
+		insert(tree,bi);
+
+		// check BA
+		Node<String> baNode = tree.find(ba);
+		assertTrue(baNode != null);
+		assertEquals(0, baNode.getChildren().size());
+		assertTrue(baNode.getParent() == null);
+
+		// check BI
+		Node<String> biNode = tree.find(bi);
+		assertTrue(biNode != null);
+		assertEquals(0, biNode.getChildren().size());
+		assertTrue(biNode.getParent() == null);
+
+		// now insert B
+		insert(tree, b);
+		assertEquals(3, tree.getNodeCount());
+
+		Node<String> bNode = tree.find(b);
+		baNode = tree.find(ba);
+		biNode = tree.find(bi);
+
+		// check B
+		assertTrue(bNode != null);
+		List<Node<String>> bChildren = bNode.getChildren();
+		assertEquals(2, bChildren.size());
+		assertTrue(bChildren.contains(biNode));
+		assertTrue(bChildren.contains(baNode));
+
+		// check BA again
+		assertTrue(baNode != null);
+		assertEquals(0, baNode.getChildren().size());
+		assertTrue(baNode.getParent().equals(bNode));
+
+		// check BI again
+		assertTrue(biNode != null);
+		assertEquals(0, biNode.getChildren().size());
+		assertTrue(biNode.getParent().equals(bNode));
+
+		// remove B
+		boolean removedB = tree.remove(b);
+		assertTrue(removedB);
+		assertFalse(tree.remove(b));
+		assertEquals(null, tree.find(b));
+
+		// check BA again
+		baNode = tree.find(ba);
+		assertTrue(baNode != null);
+		assertEquals(0, baNode.getChildren().size());
+		assertTrue(baNode.getParent() == null);
+
+		// check BI again
+		biNode = tree.find(bi);
+		assertTrue(biNode != null);
+		assertEquals(0, biNode.getChildren().size());
+		assertTrue(biNode.getParent() == null);
+
+	}
+
+	private void insert(PartialHierarchyTree<String> tree, String b) {
+		try {
+			tree.insertNode(b);
+		}
+		catch (PartialHierarchyException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
