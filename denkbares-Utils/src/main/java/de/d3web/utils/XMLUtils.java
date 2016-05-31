ece8d826bb05daeda57a6d97f0d90d174ee2f448
Alex Legler <alex.legler@denkbares.com>
2016-05-31 11:08:49 +0200

    Add XMLUtils with namespace-aware XPath support
    Code via http://stackoverflow.com/questions/6390339/how-to-query-xml-using-namespaces-in-java-with-xpath

---
+++ denkbares-Utils/src/main/java/de/d3web/utils/XMLUtils.java
@@ -0,0 +1,53 @@
+package de.d3web.utils;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.xml.namespace.NamespaceContext;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathFactory;
+
+/**
+ * XML-related utilities
+ *
+ * @author Alex Legler (denkbares GmbH)
+ * @created 2016-05-31
+ */
+public class XMLUtils {
+	/**
+	 * Creates a new {@link XPath} instance using the default {@link XPathFactory} and sets a matching
+	 * {@link NamespaceContext} that resolves namespaces
+	 *
+	 * @param namespaceMap Map of identifier -> URI namespaces to use
+	 * @return {@link XPath} aware of given namespaces
+	 */
+	public static XPath createNamespaceAwareXPath(Map<String, String> namespaceMap) {
+		final XPathFactory factory = XPathFactory.newInstance();
+		final XPath xpath = factory.newXPath();
+		final NamespaceContext namespaceContext = new SimpleNamespaceContext(namespaceMap);
+		xpath.setNamespaceContext(namespaceContext);
+
+		return xpath;
+	}
+
+	private static class SimpleNamespaceContext implements NamespaceContext {
+		private final Map<String, String> PREF_MAP = new HashMap<>();
+
+		SimpleNamespaceContext(final Map<String, String> prefMap) {
+			PREF_MAP.putAll(prefMap);
+		}
+
+		public String getNamespaceURI(String prefix) {
+			return PREF_MAP.get(prefix);
+		}
+
+		public String getPrefix(String uri) {
+			throw new UnsupportedOperationException();
+		}
+
+		public Iterator getPrefixes(String uri) {
+			throw new UnsupportedOperationException();
+		}
+	}
+}
