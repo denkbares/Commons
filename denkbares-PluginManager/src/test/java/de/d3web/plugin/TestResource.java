234d20b58790cba539f430df27a81cb32b964327
Albrecht Striffler <albrecht.striffler@denkbares.com>
2016-06-28 17:46:05 +0200

    Replaced deprecated junit.framework.Assert with org.junit.Assert and some minor cleanup.

--- denkbares-PluginManager/src/test/java/de/d3web/plugin/TestResource.java
+++ denkbares-PluginManager/src/test/java/de/d3web/plugin/TestResource.java
@@ -28,7 +28,7 @@ import java.io.Writer;
 import java.util.LinkedList;
 import java.util.List;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 
 import org.junit.Before;
 import org.junit.Test;
