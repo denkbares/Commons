d62bc8a7a52a8ddc623c71ba5930fe6025cacf17
Albrecht Striffler <albrecht.striffler@denkbares.com>
2017-06-17 14:46:13 +0200

    Implemented UsageBasedScheduler. Can be used if a task should only be run if a specific action happened recently. If that action did not happen recently, the task will be delayed till the next time it happens, or optionally, it will run at a random time coming night/morning.

---
+++ denkbares-Utils/src/main/java/com/denkbares/utils/UsageBasedScheduler.java
@@ -0,0 +1,113 @@
+package com.denkbares.utils;
+
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import static java.util.Calendar.HOUR_OF_DAY;
+import static java.util.concurrent.TimeUnit.*;
+
+/**
+ * Scheduler that can be used if a task should only be run if something happened recently. To indicate, that this
+ * "thing" happened, {@link #notifyUsage()} can be called.
+ * <p>
+ * Created by Albrecht on 15.06.2017.
+ */
+public class UsageBasedScheduler {
+
+	private final ScheduledExecutorService scheduler;
+	private String name;
+	private final ExecutorService executorService;
+	private final long triggerWithingMillis;
+	private final boolean runAtNightAnyway;
+	private final Set<Runnable> runnables = Collections.synchronizedSet(new HashSet<>());
+	private long lastUsage = Long.MIN_VALUE;
+
+	/**
+	 * Creates a new scheduler.
+	 *
+	 * @param name                 the name of this scheduler, so it can be identified easily when debugging
+	 * @param executorService      the ExecutorService running the scheduled runnable asynchronously
+	 * @param triggerWithingMillis if a runnable is scheduled within the given duration in milliseconds since the last
+	 *                             call to {@link #notifyUsage()}, it is executed immediately
+	 * @param runAtNightAnyway     decides whether a scheduled runnable should be run at night, if there wasn't any
+	 *                             call to {@link #notifyUsage()} in the mean time
+	 */
+	public UsageBasedScheduler(String name, ExecutorService executorService, long triggerWithingMillis, boolean runAtNightAnyway) {
+		this.name = name;
+		this.scheduler = Executors.newSingleThreadScheduledExecutor(r -> {
+			Thread thread = new Thread(r, name);
+			thread.setDaemon(true);
+			return thread;
+		});
+		this.executorService = executorService;
+		this.triggerWithingMillis = triggerWithingMillis;
+		this.runAtNightAnyway = runAtNightAnyway;
+	}
+
+	/**
+	 * Schedules the runnable to be executed asynchronously (call do schedule will return immediately).
+	 * If {@link #notifyUsage()} was called recently (within duration given in constructor), the runnable will be
+	 * executed immediately. If not, execution of the runnable will be delayed either until some random time at night
+	 * (if so specified in constructor), or when the methode {@link #notifyUsage()} is called the next time.
+	 *
+	 * @param runnable the runnable to be scheduled
+	 */
+	public void schedule(Runnable runnable) throws ExecutionException, InterruptedException {
+		// usage not so far back, run immediately
+		long durationSinceLastUsage = System.currentTimeMillis() - lastUsage;
+		if (lastUsage > Long.MIN_VALUE && durationSinceLastUsage < triggerWithingMillis) {
+			executorService.execute(runnable);
+		}
+		// if desired, make sure that we run some time tonight at the latest
+		else if (runAtNightAnyway) {
+			int currentHour = Calendar.getInstance().get(HOUR_OF_DAY);
+			long delay = HOURS.toMillis(24 - currentHour)  // till after midnight
+					+ MINUTES.toMillis((long) (Math.random() * 300)); // another 0 - 5 hours
+
+			scheduler.schedule(() -> {
+						// check if the runnable was already executed in #notifyUsage
+						if (runnables.remove(runnable)) {
+							executorService.execute(runnable);
+						}
+					},
+					delay, MILLISECONDS);
+
+			runnables.add(runnable);
+			Date date = new Date(System.currentTimeMillis() + delay);
+			if (lastUsage == Long.MIN_VALUE) {
+				Log.info(name + " was not yet used, scheduling to run " + date);
+			}
+			else {
+				Log.info("Last notification for " + name + " was " + Stopwatch.getDisplay(durationSinceLastUsage)
+						+ " ago, scheduling to run task " +  date);
+			}
+		}
+		// or just wait for the next usage
+		else {
+			runnables.add(runnable);
+			Log.info("Last notification for " + name + " was " + Stopwatch.getDisplay(durationSinceLastUsage)
+					+ " ago, waiting for next usage do run task.");
+		}
+	}
+
+	/**
+	 * Notifies the scheduler, that a new access/usage occurred.
+	 */
+	public void notifyUsage() throws ExecutionException, InterruptedException {
+		this.lastUsage = System.currentTimeMillis();
+		synchronized (runnables) {
+			for (Runnable runnable : runnables) {
+				executorService.execute(runnable);
+			}
+			runnables.clear();
+		}
+	}
+
+}
