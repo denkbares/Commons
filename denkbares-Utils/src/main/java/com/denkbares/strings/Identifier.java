33b6fdd978b68a2119096a0d3915b5f50b535d8f
Simon Maurer <simon.maurer@denkbares.com>
2018-01-31 14:55:42 +0100

    fixed renaming of turtle definitions without abbreviation, general cleanup of term renaming

--- denkbares-Utils/src/main/java/com/denkbares/strings/Identifier.java
+++ denkbares-Utils/src/main/java/com/denkbares/strings/Identifier.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2012 University Wuerzburg, Computer Science VI
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -316,4 +316,9 @@ public class Identifier implements Comparable<Identifier> {
 		return this.pathElements.length;
 	}
 
+	public static Identifier matchCompatibilityForm(Identifier termIdentifier) {
+		return (termIdentifier.countPathElements() == 2 && termIdentifier.getPathElementAt(0).isEmpty())
+				? new Identifier(termIdentifier.isCaseSensitive(), "lns", termIdentifier.getLastPathElement())
+				: termIdentifier;
+	}
 }
