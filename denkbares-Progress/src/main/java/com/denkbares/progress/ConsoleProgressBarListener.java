475571ca732addb6cb96032af79ef2b074ec97e2
volker.belli <volker.belli@denkbares.com>
2019-01-31 10:15:48 +0100

    improved console progress listener
     * no longer conflicting with logs and console printing
     * now displaying percentage value
     * now printing progress messages

--- denkbares-Progress/src/main/java/com/denkbares/progress/ConsoleProgressBarListener.java
+++ denkbares-Progress/src/main/java/com/denkbares/progress/ConsoleProgressBarListener.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (C) 2009 denkbares GmbH
- * 
+ *
  * This is free software; you can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 3 of the License, or (at your option) any
  * later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this software; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
@@ -20,19 +20,21 @@ package com.denkbares.progress;
 
 /**
  * A simple ProgressListener that prints the progress to the console
- * 
+ *
  * @author Volker Belli (denkbares GmbH)
  */
 @SuppressWarnings("UseOfSystemOutOrSystemErr")
 public class ConsoleProgressBarListener implements ProgressListener {
 
-	private int lastProgress = -1;
 	private static final int SIZE = 50;
+	private static final float UPDATE_DELTA = 0.001f;
+
+	private float percent = Float.NaN;
 
 	@Override
 	public void updateProgress(float percent, String message) {
-		int progress = (int) (percent * SIZE);
-		if (lastProgress == -1) {
+		if (Float.isNaN(this.percent)) {
+			System.out.println();
 			System.out.println(message);
 			System.out.print("| 0 % ");
 			for (int i = 0; i < (SIZE - 18) / 2; i++) {
@@ -43,19 +45,27 @@ public class ConsoleProgressBarListener implements ProgressListener {
 				System.out.print("\u00AF");
 			}
 			System.out.println(" 100 % |");
-			System.out.print("|");
-			lastProgress = 0;
+			message = null;
 		}
-		if (lastProgress == progress) return;
+		else if (percent < 1f && (percent <= this.percent + UPDATE_DELTA)) {
+			return;
+		}
 
-		for (int i = lastProgress + 1; i <= progress; i++) {
-			System.out.print("=");
+		this.percent = Math.min(Math.max(0f, percent), 1f);
+		int barSize = Math.round(percent * SIZE);
+		System.out.print("|");
+		for (int i = 1; i <= SIZE; i++) {
+			System.out.print(i <= barSize ? "=" : " ");
 		}
-		lastProgress = progress;
-		if (progress == SIZE) {
-			System.out.println("|");
-			System.out.println("done");
+		System.out.print("| ");
+		System.out.format("%3.1f%% ", this.percent * 100);
+		if (message != null) System.out.print(message);
+		if (this.percent >= 1f) {
+			if (message == null || message.isEmpty()) System.out.print("done");
+			System.out.println();
+		}
+		else {
+			System.out.print("\r");
 		}
 	}
-
 }
