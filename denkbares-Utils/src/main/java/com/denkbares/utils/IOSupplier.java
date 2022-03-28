6dd593507dbe38d91dc9e35d812b72b585a8c368
Volker Belli <volker.belli@denkbares.com>
2022-03-28 13:13:00 +0200

    make DataResource freely available

---
+++ denkbares-Utils/src/main/java/com/denkbares/utils/IOSupplier.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2022 denkbares GmbH, Germany
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA, or see the FSF
+ * site: http://www.fsf.org.
+ */
+
+package com.denkbares.utils;
+
+import java.io.IOException;
+
+/**
+ * Supplier which is able to throw an IOException.
+ *
+ * @author Friedrich Fell (Service Mate GmbH)
+ * @created 19.04.21
+ */
+@FunctionalInterface
+public interface IOSupplier<T> {
+
+	/**
+	 * Gets a result.
+	 *
+	 * @return a result
+	 */
+	T get() throws IOException;
+}
