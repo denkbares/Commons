91db6df380a12a4e9795657080e02d938dd5361e
Volker Belli <volker.belli@denkbares.com>
2024-04-17 09:23:21 +0200

    fix console formatted string clear function

--- denkbares-Utils/src/main/java/com/denkbares/utils/Consoles.java
+++ denkbares-Utils/src/main/java/com/denkbares/utils/Consoles.java
@@ -52,7 +52,7 @@ public class Consoles {
 	 * @return the unformatted plain text
 	 */
 	public static String toPlainText(String formattedText) {
-		return formattedText.replaceAll("\\u001b\\[\\d+(?:;\\d+)?[a-zA-Z]", "");
+		return formattedText.replaceAll("\\u001b\\[\\d+(?:;\\d+)*[a-zA-Z]", "");
 	}
 
 	@FunctionalInterface
