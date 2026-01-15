70c1d0fc086c3a75310a298793c341b69fbf4128
Stefan Olbrecht <stefan.olbrecht@servicemate.de>
2026-01-15 16:00:17 +0100

    Update documentation

--- denkbares-Utils/src/main/java/com/denkbares/collections/CacheMap.java
+++ denkbares-Utils/src/main/java/com/denkbares/collections/CacheMap.java
@@ -23,8 +23,25 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 /**
- * A LinkedHashMap which has a max size. When max size is reached, the eldest element will be removed. When adding a
- * value which already exists in the map, the old value will be deleted and the new one added as the first element.
+ * A size-limited {@link LinkedHashMap} that automatically evicts the eldest entry when the maximum size is reached.
+ * Entries are ordered from "eldest" (head of the list, first to be removed) to "newest" (tail of the list).
+ *
+ * <p>The map supports two different caching strategies determined by the {@code accessOrder} flag:</p>
+ *
+ * <ul>
+ *     <li><b>Full Least Recently Used (LRU) Mode (accessOrder = true):</b>
+ *     Any interaction with an entry - both reading ({@code get}) and writing ({@code put}) - moves that entry to the
+ *     end of the list, marking it as the "newest". Entries that are neither read nor updated will gradually move toward
+ *     the head and eventually be evicted.
+ *     <br><i>Example:</i> A database query cache. Data that is frequently requested remains in memory, even if it is
+ *     never updated.</li>
+ *     <li><b>Update-based LRU Mode (accessOrder = false):</b>
+ *     Only adding or updating an entry ({@code put}) moves it to the end of the list. Pure read access ({@code get})
+ *     does not change the order.
+ *     <br><i>Example:</i> A configuration cache where entries are only considered "fresh" if they have actually been
+ *     modified. An entry that is read often but was last changed a long time ago will eventually be evicted to make
+ *     room for truly newer data.</li>
+ * </ul>
  *
  * @author Friedrich Fell (Service Mate GmbH)
  * @created 21.01.22
@@ -50,11 +67,6 @@ public class CacheMap<K, V> extends LinkedHashMap<K, V> {
 		return size() > maxEntries;
 	}
 
-	/**
-	 * Inserts the specified key-value pair into the map. If a mapping for the key previously exists, the old value is
-	 * replaced. If the map is in accessOrder mode, the key is inserted in a manner maintaining access order. Otherwise,
-	 * the key is manually repositioned to the newest entry.
-	 */
 	@Override
 	public V put(K key, V value) {
 		if (accessOrder) {
